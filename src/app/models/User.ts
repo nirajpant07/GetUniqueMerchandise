2acc1f4c9cc95d1e8ffe5ef82427c7500314a794
nirajpant07 <nirajpant07@gmail.com>
2020-12-03 19:10:05 +0545

    User Model Updated

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,11 +1,19 @@
-export class User
-{
-    public Email : string;
-    public Password : string;
+export class User {
+    public FirstName: string;
+    public MiddleName: string;
+    public LastName: string;
+    public Email: string;
+    public Phone: string;
+    public Password: string;
+    public RoleID: number;
 
-    constructor()
-    {
-        this.Email="";
-        this.Password="";
+    constructor() {
+        this.FirstName = "";
+        this.MiddleName = "";
+        this.LastName = "";
+        this.Email = "";
+        this.Phone = "";
+        this.Password = "";
+        this.RoleID = 2;
     }
 }
